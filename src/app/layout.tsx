bd8d6fc7a969bddf5fb82f5efe7d8f5f7de999d3
TMR-kevin <kyoutae.na@tmr-founders.com>
2025-12-04 15:35:30 +0900

    MS Clarity 연동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head"
+import Script from "next/script"
 import "../styles/globals.css"
 import "../../public/assets/css/reset.css"
 import "../../public/assets/css/font.css"
@@ -17,24 +17,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="ko">
             <head>
-                <Head>
-                    <title>{metadata.title}</title>
-                    <link rel="icon" type="image/svg+xml" href="../../public/assets/img/favicon.svg" />
-                    
-                    {/* 기본 메타 태그 */}
-                    <meta name="description" content={metadata.description} />
-                    {/* <meta name="keywords" content="워크폴리오, Workfolio, 기록 플랫폼, 커리어 관리, 업무 기록, 이력 관리, 자기계발, 커리어 성장" />
-                    <meta name="author" content="Workfolio" /> */}
-                    
-                    {/* 오픈 그래프 */}
-                    {/* <meta property="og:title" content="워크폴리오 - 나만의 기록장" />
-                    <meta property="og:description" content="일과 이력을 한곳에 쌓아두는 기록 플랫폼, 워크폴리오. 경험을 정리하고 커리어를 성장시켜 보세요." />
-                    <meta property="og:type" content="website" />
-                    <meta property="og:url" content="" />
-                    <meta property="og:image" content="" /> */}
-                </Head>
+                <link rel="icon" type="image/svg+xml" href="../../public/assets/img/favicon.svg" />
             </head>
             <body>
+                <Script
+                    id="microsoft-clarity"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(c,l,a,r,i,t,y){
+                                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                            })(window, document, "clarity", "script", "ug4gm8dyl9");
+                        `,
+                    }}
+                />
                 {children}
                 <ConfirmDialogProvider />
                 <NotificationProvider />
